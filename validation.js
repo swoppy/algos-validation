e8b427acafa93590b80e35e52de613b7da43bacb
swoppy <json.barba@gmail.com>
2019-01-31 19:26:10 +0800

    Refactored, to check for falsy property then check data based on rules (regex), reduced objs to properties

--- validation.js
+++ validation.js
@@ -1,76 +1,136 @@
-const createValidation = (fn, errorMsg, type) => data => {
+const createValidation = (fn, errorMsg) => data => {
   const result = fn(data);
   return {
-    cata: branch => result ? branch.right(result, type) : branch.left(errorMsg, type), type
+    cata: branch => (result ? branch.right(result) : branch.left(errorMsg))
   };
 };
 
-const tranRequestDate = createValidation(
-  data => data.length ? !/^[\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}]*$/.test(data) : "Required parameter",
+const senderTranId = createValidation(
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 8
+        ? /^[0-9]{1,8}$/.test(data)
+        : "Must not exceed 8 characters"
+      : "Required parameter",
+  "Has invalid character(s)"
+);
+const runDate = createValidation(
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length
+        ? /^\d\d\d\d-\d\d-\d\d$/.test(data)
+        : "Required parameter"
+      : "Required parameter",
   "Invalid date format"
 );
-const senderTranId = createValidation(
-  data => data.length ?
-    data.length <= 8 ? /^[0-9]{1,8}$/.test(data) : "Must not exceed 8 characters"
-    : "Required parameter",
-  "Has invalid characters"
-);
 const description = createValidation(
-  data => data.length ?
-    data.length <= 180 ? /^[a-zA-Z0-9-\s]*$/.test(data) : "Must not exceed 180 characters"
-    : "Required parameter",
-  "Has invalid characters"
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 180
+        ? /^[a-zA-Z0-9-\s]*$/.test(data)
+        : "Must not exceed 180 characters"
+      : true,
+  "Has invalid character(s)"
 );
 const processor = createValidation(
-  data => data.length ?
-    data.length <= 4 ? /^[a-zA-Z]+$/.test(data) : "Must no excees 4 characters"
-    : "Required parameter",
-  "Has invalid characters"
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 4
+        ? /^[a-zA-Z]+$/.test(data)
+        : "Must no exceed 4 characters"
+      : "Required parameter",
+  "Has invalid character(s)"
 );
-const names = createValidation(
-  data => data.length ?
-    data.length <= 30 ? /^[a-zA-Z0-9-\s]*$/.test(data) : "Must not exceed 30 characters"
-    : "Required parameter",
-  "Has invalid characters"
+const mName = createValidation(
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 30
+        ? /^[a-zA-Z0-9-\s]*$/.test(data)
+        : "Must not exceed 30 characters"
+      : true,
+  "Has invalid character(s)"
+);
+const flName = createValidation(
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 30
+        ? /^[a-zA-Z0-9-\s]*$/.test(data)
+        : "Must not exceed 30 characters"
+      : "Required parameter",
+  "Has invalid character(s)"
 );
 const amount = createValidation(
-  data => data.length ?
-    data.length <= 18 ? /^([0-9]{0,15})(\.[0-9]{1,2})?$/.test(data) : "Must no exceed 18 characters"
-    : "Required parameter",
-  "Has invalid characters"
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 18
+        ? /^([0-9]{0,15})(\.[0-9]{1,2})?$/.test(data)
+        : "Must no exceed 18 characters"
+      : "Required parameter",
+  "Has invalid character(s)"
 );
 const currency = createValidation(
-  data => data.length ?
-    data.length <= 3 ? /^[a-zA-Z]+$/.test(data) : "Must not exceed 3 characters"
-    : "Required parameter",
-  "Has invalid characters"
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 3
+        ? /^[a-zA-Z]+$/.test(data)
+        : "Must not exceed 3 characters"
+      : "Required parameter",
+  "Has invalid character(s)"
 );
-const address = createValidation(
-  data => data.length ?
-    data.length <= 60 ? /^[a-zA-Z0-9-\s]*$/.test(data) : "Must not exceed 60 characters"
-    : "Required parameter",
-  "Has invalid characters"
+const address1 = createValidation(
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 60
+        ? /^[a-zA-Z0-9-\s]*$/.test(data)
+        : "Must not exceed 60 characters"
+      : "Required parameter",
+  "Has invalid character(s)"
+);
+const address2 = createValidation(
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 60
+        ? /^[a-zA-Z0-9-\s]*$/.test(data)
+        : "Must not exceed 60 characters"
+      : true,
+  "Has invalid character(s)"
 );
 const brgy = createValidation(
-  data => data.length ?
-    data.length <= 40 ? /^[a-zA-Z0-9-\s]*$/.test(data) : "Must not exceed 40 characters"
-    : "Required parameter",
-  "Has invalid characters"
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 40
+        ? /^[a-zA-Z0-9-\s]*$/.test(data)
+        : "Must not exceed 40 characters"
+      : "Required parameter",
+  "Has invalid character(s)"
 );
 const cityProvinceMobileNational = createValidation(
-  data => data.length ?
-    data.length <= 20 ? /^[a-zA-Z0-9-\s]*$/.test(data) : "Must not exceed 20 characters"
-    : "Required parameter",
-  "Has invalid characters"
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 20
+        ? /^[a-zA-Z0-9-\s]*$/.test(data)
+        : "Must not exceed 20 characters"
+      : "Required parameter",
+  "Has invalid character(s)"
 );
 const email = createValidation(
-  data => data.length ?
-    data.length <= 80 ? /^[a-zA-Z0-9-\s]*$/.test(data) : "Must not exceed 80 characters"
-    : "Required parameter",
-  "Has invalid characters"
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length <= 80
+        ? /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            data
+          )
+        : "Must not exceed 80 characters"
+      : "Required parameter",
+  "Invalid foramat"
 );
 const birthdate = createValidation(
-  data => data.length ? /^\d{2}([./-])\d{2}\1\d{4}$/.test(data) : "Required parameter",
+  data =>
+    data !== undefined && data !== null && data !== " " && data !== ""
+      ? data.length
+        ? /^\d\d\d\d-\d\d-\d\d$/.test(data)
+        : "Required parameter"
+      : "Required parameter",
   "Invalid date format"
 );
 
@@ -83,29 +143,35 @@ const validateRequest = values =>
   }));
 
 const requestValues = {
-  senderTranId: senderTranId('12345678'),
-  tranRequestDate: tranRequestDate('10/10/2010'),
+  senderTranId: senderTranId("12345678"),
+  tranRequestDate: runDate("2019-01-29"),
   currency: currency("PHP"),
   value: amount("1000"),
-  description: description('ascsvdfsdfs'),
-  processor: processor('UBP'),
-  first: names('Juan'),
-  middle: names('S'),
-  last: names('asd'),
-  line1: address('test'),
-  line2: address('12312'),
-  barangay: brgy('dasdasd'),
-  city: cityProvinceMobileNational('ASDASD'),
-  province: cityProvinceMobileNational('Metro Manila'),
-  email: email('testtestscom'),
-  mobile: cityProvinceMobileNational('897129837'),
-  birthdate: birthdate('10/10/2018'),
-  nationality: cityProvinceMobileNational('')
+  description: description("ascsvdfsdfs"),
+  processor: processor("UBP"),
+  first: flName("Juan"),
+  middle: mName("S"),
+  last: flName("asd"),
+  line1: address1("test"),
+  line2: address2("12312"),
+  barangay: brgy("dasdasd"),
+  city: cityProvinceMobileNational("Pasig"),
+  province: cityProvinceMobileNational("Metro Manila"),
+  email: email("testtestscom"),
+  mobile: cityProvinceMobileNational("23123123"),
+  birthdate: birthdate("10/10/2018"),
+  nationality: cityProvinceMobileNational("Marsian")
 };
 
-let errors = Object.assign(...validateRequest(requestValues))
-let filter = Object.keys(errors).reduce((p, c) => {
-  if (errors[c] !== true) p[c] = errors[c];
+let errors = validateRequest(requestValues).reduce(
+  (p, v) => Object.assign(p, v),
+  {}
+);
+let filter = Object.keys(errors).reduce((p, v) => {
+  if (errors[v] !== true) p[v] = errors[v];
   return p;
 }, {});
-let g = Object.keys(filter).length ? "404" : "200"
+let statusCode = Object.keys(filter).length ? "404" : "200";
+
+console.log(JSON.stringify(errors, null, 2));
+console.log(statusCode);
